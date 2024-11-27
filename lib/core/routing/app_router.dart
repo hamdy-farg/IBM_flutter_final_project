9a50a15d3150cf5d3938876774fa08354767426b
Youssef-Mohamed <yhmza818@gmail.com>
2024-11-27 11:42:07 +0200

    Implement workspace-status feature

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:ibm_flutter_final_project/core/routing/routes.dart';
+import 'package:ibm_flutter_final_project/features/workspace_status/ui/workspace_status.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     // this arguments to be passed in any screen like this (arguments as ClassName)
     final arguments = settings.arguments;
     switch (settings.name) {
-      //! this is how to add route and navigate by 
+      //! this is how to add route and navigate by
       //! first added your route in routes.dart
       //! context.pushNamed()
       // case Routes.onBoardingScreen:
       //   return MaterialPageRoute(
       //     builder: (_) => const OnboardingScreen(),
       //   );
+      case Routes.workspaceStatus:
+        return MaterialPageRoute(
+          builder: (_) => WorkspaceStatus(),
+        );
 
       default:
         return MaterialPageRoute(
--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,1 +1,36 @@
-class TextStyles {}
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ibm_flutter_final_project/core/theming/colors.dart';
+import 'package:ibm_flutter_final_project/core/theming/font_wight_helper.dart';
+
+class TextStyles {
+  static TextStyle font24BlackSemiBold = TextStyle(
+      fontSize: 24.sp,
+      color: Colors.black,
+      fontWeight: FontWeightHelper.semiBold);
+  static TextStyle font10WhiteRegular = TextStyle(
+      fontSize: 10.sp,
+      color: Colors.white,
+      fontWeight: FontWeightHelper.regular);
+  static TextStyle font16WhiteBold = TextStyle(
+      fontSize: 16.sp, color: Colors.white, fontWeight: FontWeightHelper.bold);
+  static TextStyle font10BlueRegular = TextStyle(
+      fontSize: 10.sp,
+      color: ColorsManager.mainBlue,
+      fontWeight: FontWeightHelper.regular);
+  static TextStyle font18WhiteBold =
+      TextStyle(fontSize: 18.sp, 
+      color: Colors.white,
+      fontWeight: FontWeight.bold);  
+      static TextStyle font14GreyRegular =
+      TextStyle(fontSize: 14.sp, 
+      color: Colors.grey,
+      fontWeight: FontWeightHelper.regular);   
+       static TextStyle fonst18BlackBold =
+      TextStyle(fontSize: 18.sp, 
+      color: Colors.black,
+      fontWeight: FontWeightHelper.bold);
+
+}
+ 
+   
